0874ac64aad2fc5e1b867c9b3bc21b1fa13a0910
GarabeitM <114006383+GarabeitM@users.noreply.github.com>
2022-10-23 19:54:44 +0200

    Added the file

---
+++ Hi.py
@@ -0,0 +1,13 @@
+print("Hi, Mary!")
+a = input("What would you like to do first today?")
+print("1- " + a)
+b = input("What would you like to do next?")
+print("2- " + b)
+c = input("What would you like to do last today?")
+print("3- " + c)
+d = input("Woud you like to do anything else?")
+if d == "No":
+    print("Ok then, see you tomorrow!")
+else:
+    e = input("can you elaburate more?")
+    print("4- " + e)
